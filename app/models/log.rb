7a39b9b7509c91d245c895f2c62456ce5d38ea2e
William Chen <williamchen16@gmail.com>
2012-12-23 21:25:06 -0600

    Model modification to reflect has_one/many

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,3 +1,5 @@
 class Log < ActiveRecord::Base
+  has_many :requests, :dependent => :destroy
+  has_many :notices,  :dependent => :destroy
   attr_accessible :ip, :token
 end
--- app/models/notice.rb
+++ app/models/notice.rb
@@ -1,5 +1,4 @@
 class Notice < ActiveRecord::Base
-  belongs_to :type
   belongs_to :log
   attr_accessible :detail
 end
