e34386370258549a0db3dd1f310eb7ef588a97a9
William Chen <williamchen16@gmail.com>
2012-12-23 20:57:05 -0600

    A wild Rails app appears!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KeynoteLogger::Application.config.secret_token = '5b40516dba270913eeb3f91a287a9942031fa096678d4c0467c408e5b56954104794311403ad8398438cb1e4883369f33c0fff5747ac47e296c422c811c0600d'
